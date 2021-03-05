ffd1f9cb4c0862bdc9578211023591474c1c2ca4
Test account <79846938+Akasuki-1@users.noreply.github.com>
2021-03-05 09:59:31 +0000

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -127,7 +127,7 @@ def start(bot: Bot, update: Update, args: List[str]):
         else:
             first_name = update.effective_user.first_name
             update.effective_message.reply_photo(DEVIL_IMG,PM_START_TEXT.format(escape_markdown(first_name), escape_markdown(bot.first_name), OWNER_NAME, OWNER_USERNAME ),reply_markup=InlineKeyboardMarkup(
-                                                [[InlineKeyboardButton(text="➕ Add To Your Group ➕",url="http://t.me/Pruthvi_RJ_Robot?startgroup=true")],
+                                                [[InlineKeyboardButton(text="➕ Add To Your Group ➕",url="http://t.me/AnjiKicchaNewBot?startgroup=true")],
                                                 [InlineKeyboardButton(text="My Owner 🇮🇳",url="https://t.me/Akboy99"),InlineKeyboardButton(text="creator ",url="https://t.me/The_NOoBHaCkeR")],  
                                                 [InlineKeyboardButton(text="Help 🤝",url="t.me/{}?start=help".format(bot.username))]]),disable_web_page_preview=True, parse_mode=ParseMode.MARKDOWN)
     else:
